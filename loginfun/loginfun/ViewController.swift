1002bc1ca21a4c4017ec8c30d5c75f15f3f577de
Sebastian Wojtyna <wojtyna.sebastian@gmail.com>
2024-12-14 14:30:59 +0100

    Commi init

---
+++ loginfun/loginfun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loginfun
+//
+//  Created by Sebastian Wojtyna on 14/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
